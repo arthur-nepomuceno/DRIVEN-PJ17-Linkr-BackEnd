4035445fdbba15aaa324bc55ac948e5295f2e4e2
TiaraOliveira <tiaragomes@yahoo.com.br>
2022-08-09 20:14:14 -0300

    implements improve isvalidEmail

--- src/middlewares/isValidEmail.js
+++ src/middlewares/isValidEmail.js
@@ -5,16 +5,25 @@ export default async function isValidEmail(req, res, next){
     const body = req.body;
     
     try{
-      
-        const {rows: email} = await getUserByEmail(body.email);
-     
-        if(email.length === 0){
+        const {rows: result} = await getUserByEmail(body.email);
+   
+        if(result.length === 0){
             return res.sendStatus(401);
         } else {
-            next();
+            const isValidPassword = passwordDecrypter(body.password, result[0].password);            
+         
+            if(!isValidPassword){
+                return res.sendStatus(401);
+            }
+
         }
 
-    } catch(error){
+        delete queryResult[0].password;
+        res.locals.query = result[0];
+       
+        next();
+
+    }catch(error){
         return res.status(500).send(error);
     }
 }
