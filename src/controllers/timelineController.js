7bc3a47d1343586a269d3b79ea553b387078afe2
Arthur Nepomuceno <arth.nepomuceno@gmail.com>
2022-08-12 16:38:13 -0300

    [feature] working on timeline

--- src/controllers/timelineController.js
+++ src/controllers/timelineController.js
@@ -1,10 +1,23 @@
-import { getPosts } from "../repositories/postsRepository.js"
+import { getPosts, getPostsLikesList } from "../repositories/postsRepository.js"
 
 export default async function timeline(req, res){
     try {
         const {rows: posts} = await getPosts();
-        return res.status(200).send(posts);
+        const {rows: likesList} = await getPostsLikesList();
+        
+        let likes = []
+        posts.map(post => {
+            let names = []
+            likesList.map(like => {
+                if(post.id === like.postId){
+                    names.push(like.userName)
+                }
+            })
+            likes.push(names);
+        })
+
+        return res.status(200).send(likes);
     } catch(error) {
         return res.status(500).send(error)
     }
-}
+}  
--- src/repositories/postsRepository.js
+++ src/repositories/postsRepository.js
@@ -1,8 +1,34 @@
 import db from "../database/postgres.js";
 
 function getPosts(){
-    const query= `SELECT "userName", "pictureUrl", content, url FROM posts JOIN users ON users.id = posts."userId";`
+    const query = (`SELECT posts.id, 
+                        users."userName", 
+                        users."pictureUrl" AS "userImage", 
+                        posts.content AS "postDescription", 
+                        posts.url AS "postUrl", 
+                        COUNT(likes."postId") AS "likesCount"
+                    FROM posts 
+                    JOIN users 
+                    ON users.id = posts."userId"
+                    LEFT JOIN likes
+                    ON likes."postId" = posts.id
+                    GROUP BY posts.id, 
+                        users."userName", 
+                        users."pictureUrl", 
+                        posts.content, 
+                        posts.url, 
+                        posts."createdAt"
+                    ORDER BY posts."createdAt";`)
     return db.query(query);
 }
 
-export { getPosts }
+function getPostsLikesList(postId){
+    const query = (`SELECT likes."postId", users."userName"
+                    FROM likes
+                    JOIN users
+                    ON users.id = likes."userId"
+                    ORDER BY likes."postId" ASC;`)
+    return db.query(query);
+}
+
+export { getPosts, getPostsLikesList }
