4a07aa9610c0a6eac694608b1c33d1d915ffa97e
Arthur Nepomuceno <102621683+arthur-nepomuceno@users.noreply.github.com>
2022-08-15 12:44:30 -0300

    Update postRouter.js

--- src/routes/postRouter.js
+++ src/routes/postRouter.js
@@ -1,17 +1,14 @@
 import { Router } from "express";
 import { schemaValidator } from "../middlewares/schemaValidator.js";
-import publishSchema from "../schemas/publishSchema.js";
 import updateSchema from "../schemas/updateSchema.js";
 import tokenValidator from "../middlewares/tokenValidator.js"
 import updateValidator from "../middlewares/updateValidator.js";
 import deleteValidator from "../middlewares/deleteValidator.js";
-import publishController from "../controllers/publishController.js";
 import updatePost from "../controllers/updateController.js";
 import deletePost from "../controllers/deleteController.js";
 
 const postRouter = Router();
 
-postRouter.post('/publish',schemaValidator(publishSchema), tokenValidator, publishController);
 postRouter.put('/update', schemaValidator(updateSchema), tokenValidator , updateValidator ,updatePost);
 postRouter.delete('/delete/:id', tokenValidator, deleteValidator ,deletePost);
 
