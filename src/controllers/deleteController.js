331ff9f32482dc560099dacbfda5c83778bcb816
Arthur Nepomuceno <arth.nepomuceno@gmail.com>
2022-08-16 13:51:17 -0300

    [fix] fixing publish route

--- src/controllers/deleteController.js
+++ src/controllers/deleteController.js
@@ -1,15 +1,11 @@
-
-import { deletePostById, deletePostsHashtagsById } from "../repositories/deleteRepository.js";
-
+import { deletePostById, deletePostHashtagsById } from "../repositories/deleteRepository.js";
 
 export default async function deletePost(req, res){
     const { id } = req.params;
 
     try {
-
-        await deletePostById(id);
-        await deletePostsHashtagsById(id);
-
+        await deletePostById(id)
+        await deletePostHashtagsById(id)
         return res.sendStatus(200);
     } catch(error) {
         return res.status(500).send(error);
--- src/routes/publishRouter.js
+++ src/routes/publishRouter.js
@@ -3,16 +3,6 @@ import tokenValidator from "../middlewares/tokenValidator.js";
 import publishController from "../controllers/publishController.js";
 import { schemaValidator } from "../middlewares/schemaValidator.js";
 import publishSchema from "../schemas/publishSchema.js";
-<<<<<<< HEAD
-import updateController from "../controllers/updateController.js";
-import updateSchema from "../schemas/updateSchema.js";
-import updateValidator from "../middlewares/updateValidator.js";
-
-const publishRouter = Router();
-
-publishRouter.post('/publish',schemaValidator(publishSchema), tokenValidator, publishController);
-publishRouter.post('/publish/:id', schemaValidator(updateSchema), tokenValidator , updateValidator ,updateController);
-=======
 import { hashtagChecker } from "../middlewares/hashtagValidator.js";
 import updateController from "../controllers/updateController.js";
 import updateSchema from "../schemas/updateSchema.js";
@@ -20,9 +10,7 @@ import updateValidator from "../middlewares/updateValidator.js";
 
 const publishRouter = Router();
 
-publishRouter.post('/publish',schemaValidator(publishSchema), tokenValidator, publishController);
-publishRouter.post('/publish/:id', schemaValidator(updateSchema), tokenValidator , updateValidator ,updateController);
 publishRouter.post('/publish',schemaValidator(publishSchema), tokenValidator, hashtagChecker, publishController);
->>>>>>> 14faf14c0fc4f8a23c33dfb7e62a1608c50dcdf6
+publishRouter.post('/publish/:id', schemaValidator(updateSchema), tokenValidator , updateValidator ,updateController);
 
 export default publishRouter;
